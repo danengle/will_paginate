d878daf0bfe4745e7ac16f989cf2afdd964d5be1
Dan Engle <dan@griptechnology.com>
2009-03-09 19:00:25 -0400

    added first_page? and last_page? methods to WillPaginate::Collection

--- lib/will_paginate/collection.rb
+++ lib/will_paginate/collection.rb
@@ -142,5 +142,13 @@ module WillPaginate
 
       result
     end
+    
+    def first_page?
+      self.current_page == 1
+    end
+    
+    def last_page?
+      self.current_page == self.total_pages
+    end
   end
 end
